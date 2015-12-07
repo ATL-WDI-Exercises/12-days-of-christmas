3ee4a55b240d73d7661885e2d77cc4ec95934e23
Mike Hopper <drmikeh@gmail.com>
2015-12-07 12:44:58 -0500

    Initial commit.

---
+++ lyrics.js
@@ -0,0 +1,46 @@
+var days = [
+  'first',
+  'second',
+  'third',
+  'fourth',
+  'fifth',
+  'sixth',
+  'seventh',
+  'eighth',
+  'ninth',
+  'tenth',
+  'eleventh',
+  'twelfth'
+];
+
+var gifts = [
+  'Partridge in a Pear Tree',
+  'Two Turtle Doves',
+  'Three French Hens',
+  'Four Calling Birds',
+  'Five Golden Rings',
+  'Six Geese a Laying',
+  'Seven Swans a Swimming',
+  'Eight Maids a Milking',
+  'Nine Ladies Dancing',
+  'Ten Lords a Leaping',
+  'Eleven Pipers Piping',
+  'Twelve Drummers Drumming',
+];
+
+var lastDayPrefix = 'A';
+
+for (var day=1; day <= 12; day++) {
+  console.log();
+  console.log('On the ' + days[day-1] + ' day of Christmas');
+  console.log('my true love sent to me:');
+  for (var g=day-1; g >= 0; g--) {
+    if (g === 0) {
+      console.log(lastDayPrefix + ' ' + gifts[g]);
+    }
+    else {
+      console.log(gifts[g]);
+    }
+  }
+  lastDayPrefix = 'and a';
+}
